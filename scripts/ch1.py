9a544f1fedc512071e51765963fc69a17054a986
zhuty <13718054285@163.com>
2021-07-09 15:24:40 +0800

    游戏编写

--- scripts/ch1.py
+++ scripts/ch1.py
@@ -1,30 +1,13 @@
 from ch1_choices import *
 
 
-class s1_1:
-    def choices(self):
-        ls = []
-        tmp = c1_1_1()
-        if tmp.show():
-            ls.append(tmp)
-        tmp = c1_1_2()
-        if tmp.show():
-            ls.append(tmp)
-        tmp = c1_1_3()
-        if tmp.show():
-            ls.append(tmp)
-        return ls
+class s1_1(ek_section):
+    cho = [c1_1_1(), c1_1_2(), c1_1_3()]
 
 
-class s1_2:
-    def choices(self):
-        return []
+class s1_2(ek_section):
+    cho = [c1_2_1(), c1_2_2()]
 
 
-class s1_4:
-    def choices(self):
-        ls = []
-        tmp = c1_4_1()
-        if tmp.show():
-            ls.append(tmp)
-        return ls
+class s1_4(ek_section):
+    cho = [c1_4_1()]
--- scripts/ch1_choices.py
+++ scripts/ch1_choices.py
@@ -1,29 +1,38 @@
 from ek_abstract import *
+from constant import *
 
 
 class c1_1_1(ek_choice):
     target = "1-2"
 
     def show(self):
-        return (ek_choice.kernel.paras["osl"] >= 1)
+        return (ek_choice.kernel.paras[BRUCE_STORY_LINE] >= 1)
 
 
 class c1_1_2(ek_choice):
     target = "1-3"
 
     def show(self):
-        return (ek_choice.kernel.paras["bsl"] >= 1)
+        return (ek_choice.kernel.paras[OLIVER_STORY_LINE] >= 1)
 
 
 class c1_1_3(ek_choice):
     target = "1-4"
 
     def show(self):
-        return (ek_choice.kernel.paras["ssl"] >= 1)
+        return (ek_choice.kernel.paras[SINESTRO_STORY_LINE] >= 1)
+
+
+class c1_2_1(ek_choice):
+    target = "1-5"
+
+
+class c1_2_2(ek_choice):
+    target = "1-3"
 
 
 class c1_4_1(ek_choice):
     target = "1-2"
 
     def show(self):
-        return (ek_choice.kernel.paras["osl"] >= 1)
+        return False
