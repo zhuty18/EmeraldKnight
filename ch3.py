2e306e9d6daf80b97a5aaf83b23775e1585b2603
zhuty <13718054285@163.com>
2022-01-29 16:11:11 +0800

    第三章代码

--- ch3.py
+++ ch3.py
@@ -1,2 +1,54 @@
 from ch3_choices import *
 from abstract import scene_abstract
+
+
+class s3_1(scene_abstract):
+    options = [c3_1_1, c3_1_2]
+
+
+class s3_5(scene_abstract):
+    options = [c3_5_1, c3_5_2]
+
+
+class s3_6(scene_abstract):
+    options = [c3_6_1, c3_6_2]
+
+
+class s3_8(scene_abstract):
+    options = [c3_8_1, c3_8_2]
+
+
+class s3_10(scene_abstract):
+    options = [c3_10_1, c3_10_2]
+
+
+class s3_12(scene_abstract):
+    options = [c3_12_1, c3_12_2]
+
+
+class s3_16(scene_abstract):
+    options = [c3_16_1, c3_16_2]
+
+
+class s3_18(scene_abstract):
+    options = [c3_18_1, c3_18_2, c3_18_3]
+
+
+class s3_19(scene_abstract):
+    options = [c3_19_1, c3_19_2]
+
+
+class s3_20(scene_abstract):
+    options = [c3_20_1, c3_20_2]
+
+
+class s3_21(scene_abstract):
+    options = [c3_21_1, c3_21_2]
+
+
+class s3_22(scene_abstract):
+    options = [c3_22_1, c3_22_2]
+
+
+class s3_23(scene_abstract):
+    options = [c3_23_1, c3_23_2]
--- ch3_choices.py
+++ ch3_choices.py
@@ -1,2 +1,174 @@
 from constant import *
 from abstract import choice_abstract
+
+
+class c3_1_1(choice_abstract):
+    target = "3-5"
+
+    def chosen(self):
+        gk.core.paras[SINESTRO_TAME] += 1
+        return super().chosen()
+
+
+class c3_1_2(choice_abstract):
+    target = "3-30"
+
+
+class c3_5_1(choice_abstract):
+    target = "3-6"
+
+
+class c3_5_2(choice_abstract):
+    target = "3-7"
+
+
+class c3_6_1(choice_abstract):
+    target = "3-8"
+
+
+class c3_6_2(choice_abstract):
+    target = "3-9"
+
+
+class c3_8_1(choice_abstract):
+    target = "3-10"
+
+
+class c3_8_2(choice_abstract):
+    target = "3-11"
+
+
+class c3_10_1(choice_abstract):
+    target = "3-12"
+
+
+class c3_10_2(choice_abstract):
+    target = "3-13"
+
+
+class c3_12_1(choice_abstract):
+    target = "3-14"
+
+    def chosen(self):
+        gk.core.paras[SINESTRO_TAME] += 2
+        return super().chosen()
+
+
+class c3_12_2(choice_abstract):
+    target = "3-15"
+
+
+class c3_14_1(choice_abstract):
+    target = "3-16"
+
+
+class c3_16_1(choice_abstract):
+    target = "3-17"
+
+
+class c3_16_2(choice_abstract):
+    target = "end-5"
+
+    def text(self):
+        return "直接跳"
+
+
+class c3_17_1(choice_abstract):
+    target = "3-18"
+
+
+class c3_18_1(choice_abstract):
+    target = "3-19"
+
+
+class c3_18_2(choice_abstract):
+    target = "3-20"
+
+
+class c3_18_3(choice_abstract):
+    target = "3-21"
+
+
+class c3_19_1(choice_abstract):
+    target = "3-27"
+
+    def text(self):
+        return "我捡到了他的信"
+
+
+class c3_19_2(choice_abstract):
+    target = "3-22"
+
+    def text(self):
+        return "我以为他会在山上"
+
+
+class c3_20_1(choice_abstract):
+    target = "3-22"
+
+    def text(self):
+        return "瑟尔是谁"
+
+
+class c3_20_2(choice_abstract):
+    target = "3-21"
+
+    def text(self):
+        return "阿琳是谁"
+
+
+class c3_21_1(choice_abstract):
+    target = "3-27"
+
+
+class c3_21_2(choice_abstract):
+    target = "3-28"
+
+
+class c3_22_1(choice_abstract):
+    target = "3-23"
+
+
+class c3_22_2(choice_abstract):
+    target = "3-24"
+
+
+class c3_23_1(choice_abstract):
+    target = "3-25"
+
+
+class c3_23_2(choice_abstract):
+    target = "3-26"
+
+    def show(self):
+        return gk.core.paras[SINESTRO_TAME] == 6
+
+    def chosen(self):
+        gk.core.paras[SINESTRO_LOVE] += 3
+        return super().chosen()
+
+
+class c3_24_1(choice_abstract):
+    target = "3-23"
+
+    def text(self):
+        return "怎么说？"
+
+
+class c3_27_1(choice_abstract):
+    target = "3-23"
+
+    def text(self):
+        return "信上的瑟尔又是谁？"
+
+
+class c3_28_1(choice_abstract):
+    target = "3-29"
+
+
+class c3_29_1(choice_abstract):
+    target = "4-1"
+
+
+class c3_30_1(choice_abstract):
+    target = "3-2"
