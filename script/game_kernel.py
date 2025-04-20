819bc2b941e8be9065ae2fb4ca4f61f7cbfd1712
tuzicao <13718054285@163.com>
2025-04-20 14:18:30 +0800

    第二章重构

--- script/game_kernel.py
+++ script/game_kernel.py
@@ -32,18 +32,14 @@ class Choice:
         """是否显示"""
         if not "show" in self._data:
             return True
-        res = True if self._data["show"] == "AND" else False
-        index = 0
-        while True:
-            index += 1
-            if not f"show_action_{index}" in self._data:
-                break
+        res = True if self._data["show"]["op"] == "AND" else False
+        for condition in self._data["show"]["condition"]:
             check = Kernel.KERNEL.check_condition(
-                self._data[f"show_para_{index}"],
-                self._data[f"show_action_{index}"],
-                self._data[f"show_value_{index}"],
+                condition["para"],
+                condition["check"],
+                condition["value"],
             )
-            match self._data["show"]:
+            match self._data["show"]["op"]:
                 case "AND":
                     res &= check
                 case "OR":
@@ -52,16 +48,13 @@ class Choice:
 
     def chosen(self):
         """选择后"""
-        index = 0
-        while True:
-            index += 1
-            if not f"action_{index}" in self._data:
-                break
-            Kernel.KERNEL.change_para(
-                self._data[f"change_para_{index}"],
-                self._data[f"action_{index}"],
-                self._data[f"change_value_{index}"],
-            )
+        if "choose" in self._data:
+            for action in self._data["choose"]:
+                Kernel.KERNEL.change_para(
+                    action["para"],
+                    action["change"],
+                    action["value"],
+                )
         Kernel.KERNEL.to_scene(self._data["target"])
 
 
@@ -72,7 +65,7 @@ class Scene:
     def get_by_id(scene_id):
         """通过ID获取场景"""
         if "end" in scene_id:
-            Kernel.open_end(scene_id)
+            Kernel.mark_end(scene_id)
             return Scene(
                 {
                     "id": scene_id,
@@ -90,7 +83,14 @@ class Scene:
 
     def get_text(self):
         """获取文本"""
-        return Kernel.read_file("story", f"{self._data["id"]}.txt", False)
+        scene_text = Kernel.read_file("story", f"{self._data["id"]}.txt", False)
+        if "end" in self._data["id"]:
+            scene_text += Kernel.STORY_END + Kernel.get_end_name(
+                self._data["id"]
+            )
+        scene_text = "    " + scene_text
+        scene_text = scene_text.replace("\n", "\n    ")
+        return scene_text
 
     def get_options(self):
         """获取选项"""
@@ -109,13 +109,13 @@ class Kernel:
 
     KERNEL = None  # 游戏内核实例
 
-    CHAPTER = 1  # 章节数
+    CHAPTER = 2  # 章节数
     PATH_GAME = "game"  # 游戏相关文件路径
     FILE_PARAS = "paras.json"  # 参数存储文件
     FILE_SCENES = "scenes_ch{ch}.json"  # 场景存储文件
     FILE_CHOICES = "choices_ch{ch}.json"  # 选项存储文件
+    FILE_NAMES = "names.json"
     PATH_STORY = "story"  # 故事相关文件路径
-    FILE_NAMES = "menu.json"
     PATH_SAVE = "save"
     FILE_DEFAULT_SAVE = "0.eks"
 
@@ -124,7 +124,6 @@ class Kernel:
     DEFAULT_CONSTS = {}  # 常量表
     SCENE_MAP = {}  # 场景表
     CHOICE_MAP = {}  # 选项表
-    SCENE_NAME_MAP = {}  # 场景名表
     END_NAME_MAP = {}  # 结局名表
     CHAPTER_NAME_MAP = {}  # 章名表
 
@@ -132,6 +131,8 @@ class Kernel:
     START_OVER = ""
     SCENE = ""
     EMPTY_SAVE = ""
+    END = ""
+    STORY_END = ""
 
     @staticmethod
     def res_path(file_dir, file_name=None):
@@ -161,7 +162,34 @@ class Kernel:
     @staticmethod
     def scene_name(scene_id):
         """获取场景名"""
-        return Kernel.SCENE_NAME_MAP[scene_id]
+        return Kernel.SCENE_MAP[scene_id]["name"]
+
+    @staticmethod
+    def mark_end(end_id):
+        """解锁结局"""
+        static_save = Kernel.read_file(
+            Kernel.PATH_SAVE, Kernel.FILE_DEFAULT_SAVE
+        )
+        static_save[end_id] = 1
+        with open(
+            Kernel.res_path(Kernel.PATH_SAVE, Kernel.FILE_DEFAULT_SAVE),
+            "w",
+            encoding="utf8",
+        ) as f:
+            f.write(json.dumps(static_save, ensure_ascii=False))
+
+    @staticmethod
+    def check_end(end_id):
+        """检查结局是否解锁"""
+        static_save = Kernel.read_file(
+            Kernel.PATH_SAVE, Kernel.FILE_DEFAULT_SAVE
+        )
+        return static_save.get(end_id, 0) == 1
+
+    @staticmethod
+    def get_end_name(end_id):
+        """获取结局名称"""
+        return Kernel.END_NAME_MAP[end_id]
 
     def __init__(self):
         """初始化游戏内核"""
@@ -178,6 +206,11 @@ class Kernel:
             "code_list"
         ]:
             Kernel.DEFAULT_CODES[i["name"]] = i["value"]
+
+        end_scene = Kernel.DEFAULT_CONSTS["END_SCENE"]
+        Kernel.SCENE_MAP[end_scene["id"]] = end_scene
+        end_choice = Kernel.DEFAULT_CONSTS["END_CHOICE"]
+        Kernel.CHOICE_MAP[end_choice["id"]] = end_choice
         for ch in range(Kernel.CHAPTER):
             for i in Kernel.read_file(
                 Kernel.PATH_GAME,
@@ -189,15 +222,12 @@ class Kernel:
                 Kernel.FILE_CHOICES.replace("{ch}", str(ch + 1)),
             ):
                 Kernel.CHOICE_MAP[i["id"]] = i
-        for k, v in Kernel.read_file(Kernel.PATH_STORY, Kernel.FILE_NAMES)[
-            "scene_names"
-        ].items():
-            Kernel.SCENE_NAME_MAP[k] = v
-        for k, v in Kernel.read_file(Kernel.PATH_STORY, Kernel.FILE_NAMES)[
+
+        for k, v in Kernel.read_file(Kernel.PATH_GAME, Kernel.FILE_NAMES)[
             "end_names"
         ].items():
             Kernel.END_NAME_MAP[k] = v
-        for k, v in Kernel.read_file(Kernel.PATH_STORY, Kernel.FILE_NAMES)[
+        for k, v in Kernel.read_file(Kernel.PATH_GAME, Kernel.FILE_NAMES)[
             "chapter_names"
         ].items():
             Kernel.CHAPTER_NAME_MAP[k] = v
@@ -206,6 +236,8 @@ class Kernel:
         Kernel.START_OVER = Kernel.DEFAULT_CONSTS["START_OVER"]
         Kernel.SCENE = Kernel.DEFAULT_CONSTS["SCENE"]
         Kernel.EMPTY_SAVE = Kernel.DEFAULT_CONSTS["EMPTY_SAVE"]
+        Kernel.END = Kernel.DEFAULT_CONSTS["END"]
+        Kernel.STORY_END = Kernel.DEFAULT_CONSTS["STORY_END"]
 
         self._scene = None  # 当前场景
         self._paras = {}  # 参数存储
@@ -252,7 +284,7 @@ class Kernel:
 
     def save_at(self, save_id):
         """保存存档"""
-        save_file = Kernel.read_file(Kernel.PATH_SAVE, f"{save_id}.eks")
+        save_file = Kernel.res_path(Kernel.PATH_SAVE, f"{save_id}.eks")
         with open(save_file, "w", encoding="utf-8") as f:
             f.write(
                 json.dumps({"scene": self.get_scene_id(), "paras": self._paras})
@@ -289,13 +321,16 @@ class Kernel:
         """条件检测"""
         if para_name == Kernel.SCENE:
             check_para = self.get_scene_id()
-        else:
+        elif para_name != Kernel.END:
             para_id = Kernel.DEFAULT_PARAS[para_name]["id"]
             check_para = self._paras[para_id]
+        else:
+            check_para = para_name
         if isinstance(check_by, str) and "CODE" in check_by:
             value = Kernel.DEFAULT_CODES[check_by]
         else:
             value = check_by
+
         match check_act:
             case "EQUAL":
                 return check_para == value
@@ -309,6 +344,12 @@ class Kernel:
                 return check_para < value
             case "LESS_EQUAL":
                 return check_para <= value
+            case "BINARY":
+                return check_para >> (value - 1) & 1 == 1
+            case "NON_BINARY":
+                return check_para >> (value - 1) & 1 == 0
+            case "CHECK_END":
+                return Kernel.check_end(f"end-{value}")
 
     def get_scene_id(self):
         """获取当前场景ID"""
@@ -323,20 +364,6 @@ class Kernel:
     def get_choices(self):
         """获取选项"""
         return self._scene.get_options()
-
-    @staticmethod
-    def open_end(end_id):
-        """解锁结局"""
-        static_save = Kernel.read_file(
-            Kernel.PATH_SAVE, Kernel.FILE_DEFAULT_SAVE
-        )
-        static_save[end_id] = 1
-        with open(
-            Kernel.res_path(Kernel.PATH_SAVE, Kernel.FILE_DEFAULT_SAVE),
-            "w",
-            encoding="utf8",
-        ) as f:
-            f.write(json.dumps(static_save, ensure_ascii=False))
 
     def print_debug(self):
         """打印调试信息"""
