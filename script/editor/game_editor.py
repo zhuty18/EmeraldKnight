28e1d2c4f0a2694c28cb83754844dc119c91c72f
tuzicao <13718054285@163.com>
2025-04-21 18:48:00 +0800

    准备写个引擎

---
+++ script/editor/game_editor.py
@@ -0,0 +1,17 @@
+# coding = utf-8
+
+"""游戏编辑器"""
+
+
+class GameEditor:
+    """游戏编辑器"""
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        """开始运行"""
+
+
+if __name__ == "__main__":
+    GameEditor().run()
---
+++ script/editor/parameter_processor.py
@@ -0,0 +1,9 @@
+# coding = utf-8
+
+"""参数处理器"""
+
+from script.game_logic import Logic
+
+
+class ParameterProcessor:
+    """参数处理器"""
