bea0a3fe6498405b3ef1cea460e7e9c5ff4b430f
tuzicao <13718054285@163.com>
2025-04-24 18:35:52 +0800

    fix str

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-    base: './', // 保证相对路径在 GitHub Pages 上能正常工作
-});
+    base: './',
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'data/*',    // 你项目里的 data 文件夹
+                    dest: 'data'      // 输出到 dist/data/
+                }
+            ]
+        })
+    ],
+})
