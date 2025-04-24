1382b2838dad1a68eb36ffcf63ffa1e9f0d84739
tuzicao <13718054285@163.com>
2025-04-24 18:43:11 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-    base: './',
+    base: '/EmeraldKnight/',
     plugins: [
         viteStaticCopy({
             targets: [
