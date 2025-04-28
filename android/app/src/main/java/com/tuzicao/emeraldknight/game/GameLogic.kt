b0d7ccfff31d5177739e967e54dab39fdd3e70a4
tuzicao <13718054285@163.com>
2025-04-28 18:49:35 +0800

    完成数据打包

--- android/app/src/main/java/com/tuzicao/emeraldknight/game/GameLogic.kt
+++ android/app/src/main/java/com/tuzicao/emeraldknight/game/GameLogic.kt
@@ -1,55 +1,33 @@
 package com.tuzicao.emeraldknight.game
 
 import android.content.Context
-import com.tuzicao.emeraldknight.R
 import org.json.JSONArray
 import org.json.JSONObject
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserFactory
 
 class GameLogic {
     companion object {
-        lateinit var ChoiceMap:HashMap<String,JSONObject>
-        fun readJSONObject(context: Context,file_name:String):JSONObject{
-            return JSONObject(context.assets.open(file_name).bufferedReader().use { it.readText() })
+        lateinit var ChoiceMap: HashMap<String, JSONObject>
+        fun readJSON(context: Context, fileName: String): String {
+            return context.assets.open(fileName).bufferedReader().use { it.readText() }
         }
-        fun readJSONArray(context: Context,file_name:String): JSONArray {
-            return JSONArray(context.assets.open(file_name).bufferedReader().use { it.readText() })
-        }
-        fun init(context: Context){
-            var choices= readJSONArray(context,"choices.json")
-            for(i in 0 until  choices.length()){
-                var item=choices.getJSONObject(i)
-                GameLogic.ChoiceMap.put(item.getString("id"),item)
+
+        fun init(context: Context) {
+            val choices = JSONArray(readJSON(context, "choices.json"))
+            for (i in 0 until choices.length()) {
+                val item = choices.getJSONObject(i)
+                GameLogic.ChoiceMap[item.getString("id")] = item
             }
         }
-        fun getSceneText(context:Context, sceneId: String): String? {
-            try {
-                val inputStream = context.resources.openRawResource(R.raw.scene_text)
-                val parser = XmlPullParserFactory.newInstance().newPullParser()
-                parser.setInput(inputStream, null)
 
-                var eventType = parser.eventType
-                var sceneText: String? = null
-                var currentSceneId = ""
-
-                while (eventType != XmlPullParser.END_DOCUMENT) {
-                    when (eventType) {
-                        XmlPullParser.START_TAG -> {
-                            if (parser.name == "scene") {
-                                currentSceneId = parser.getAttributeValue(null, "id")
-                            } else if (parser.name == "text" && currentSceneId == sceneId) {
-                                sceneText = parser.nextText()
-                            }
-                        }
-                    }
-                    eventType = parser.next()
+        fun getSceneText(context: Context, sceneID: String): String {
+            val sceneTexts = JSONArray(readJSON(context, "scene_text.json"))
+            for (i in 0 until sceneTexts.length()) {
+                val item = sceneTexts.getJSONObject(i)
+                if (item.getString("id").equals(sceneID)) {
+                    return item.getString("text")
                 }
-                return sceneText
-            } catch (e: Exception) {
-                e.printStackTrace()
-                return null
             }
+            return ""
         }
     }
 }
--- android/pack_data.py
+++ android/pack_data.py
@@ -2,21 +2,65 @@
 
 """打包数据以供android发布"""
 
-# TODO: 所有选项打包为一个列表，存储在app/src/main/asset/choices.json里
-# TODO: 所有场景打包为一个列表，存储在app/src/main/asset/scenes.json里
+import json
+import os
 
-# TODO: 所有场景文本打包为一个xml，存储在app/src/main/res/raw/scene_text.xml
-# <scenes>
-#     <scene id="1-1">
-#         <text>这是场景1的文本</text>
-#     </scene>
-#     <scene id="1-2">
-#         <text>这是场景2的文本</text>
-#     </scene>
-#     <scene id="1-3">
-#         <text>这是场景3的文本</text>
-#     </scene>
-# </scenes>
+if __name__ == "__main__":
+    # 所有选项打包为一个列表，存储在app/src/main/asset/choices.json里
+    # 所有场景打包为一个列表，存储在app/src/main/asset/scenes.json里
+    with open("../data/info.json", "r", encoding="utf-8") as f:
+        info = json.loads(f.read())
+    scenes = []
+    choices = []
+    for ch in range(1, info["chapters"] + 1):
+        with open(
+            f"../data/chapter/choices_ch{ch}.json", "r", encoding="utf-8"
+        ) as f:
+            choices.extend(json.loads(f.read()))
+        with open(
+            f"../data/chapter/scenes_ch{ch}.json", "r", encoding="utf-8"
+        ) as f:
+            scenes.extend(json.loads(f.read()))
+    if not os.path.exists("app/src/main/asset"):
+        os.mkdir("app/src/main/asset")
+    with open("app/src/main/asset/choices.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(choices, ensure_ascii=False, indent=4))
+    with open("app/src/main/asset/scenes.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(scenes, ensure_ascii=False, indent=4))
 
-# TODO: 其他json配置文件移动到app/src/main/asset
-# TODO: 图标文件移动到app/src/main/res/drawable
+    # 所有场景文本打包为一个列表，存储在app/src/main/asset/scene_text.json
+    # [{"id":"场景id","text":"场景文本"}]
+    scene_text = []
+    chapter_list = os.listdir("../data/story")
+    for ch in chapter_list:
+        scene_list = os.listdir(f"../data/story/{ch}")
+        for i in scene_list:
+            if i.endswith(".txt"):
+                with open(
+                    f"../data/story/{ch}/{i}", "r", encoding="utf-8"
+                ) as f:
+                    scene_text.append({"id": i[:-4], "text": f.read()})
+    with open("app/src/main/asset/scene_text.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(scene_text, ensure_ascii=False, indent=4))
+
+    # 其他json配置文件移动到app/src/main/asset
+    json_files = os.listdir("../data")
+    json_files = {x if x.endswith(".json") else None for x in json_files}
+    if None in json_files:
+        json_files.remove(None)
+    for file in json_files:
+        os.system(
+            f"cp {os.path.join("../data",file)} {os.path.join("app/src/main/asset",file)}"
+        )
+
+    # 图标文件移动到app/src/main/res/drawable
+    icon_files = os.listdir("../data")
+    icon_files = {x if x.endswith(".png") else None for x in icon_files}
+    if None in icon_files:
+        icon_files.remove(None)
+    if not os.path.exists("app/src/main/res/drawable"):
+        os.mkdir("app/src/main/res/drawable")
+    for file in icon_files:
+        os.system(
+            f"cp {os.path.join("../data",file)} {os.path.join("app/src/main/res/drawable",file)}"
+        )
