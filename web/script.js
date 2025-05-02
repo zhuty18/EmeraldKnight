a5492b031f04d177536be9545460ca06c8176253
tuzicao <13718054285@163.com>
2025-05-02 14:59:36 +0800

    成功应用主题

--- web/script.js
+++ web/script.js
@@ -1,15 +1,37 @@
-let storyEl = document.getElementById("story");
-let inputEl = document.getElementById("playerInput");
+// let storyEl = document.getElementById("story");
+// let inputEl = document.getElementById("playerInput");
 
-function handleInput () {
-    const choice = inputEl.value.trim().toLowerCase();
-    inputEl.value = "";
+// function handleInput () {
+//     const choice = inputEl.value.trim().toLowerCase();
+//     inputEl.value = "";
 
-    if (choice === "进入") {
-        storyEl.textContent = "你迈入森林，四周变得昏暗。远处传来狼嚎...";
-    } else if (choice === "离开") {
-        storyEl.textContent = "你转身离开，但心中有些许遗憾。也许，这是个错误的决定。";
-    } else {
-        storyEl.textContent = `你说了“${choice}”，但风似乎没有回应。试试别的？`;
+//     if (choice === "进入") {
+//         storyEl.textContent = "你迈入森林，四周变得昏暗。远处传来狼嚎...";
+//     } else if (choice === "离开") {
+//         storyEl.textContent = "你转身离开，但心中有些许遗憾。也许，这是个错误的决定。";
+//     } else {
+//         storyEl.textContent = `你说了“${choice}”，但风似乎没有回应。试试别的？`;
+//     }
+// }
+
+import "./app.css"
+
+let paras = {}
+let scene = ""
+
+fetch("data/paras.json").then((res) => res.json()).then((data) => {
+    for (let i = 0; i < data.para_list.length; i++) {
+        paras[data.para_list[i].name] = data.para_list[i].default_value
     }
+})
+
+function chapter_name (scene) {
+    return "第一章"+scene
 }
+
+
+function refresh_story () {
+    document.getElementById("scene_title").textContent = chapter_name(scene)
+}
+
+refresh_story()
