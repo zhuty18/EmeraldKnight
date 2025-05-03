8d47f625f062b7fc84373b7598d41e39af25c565
tuzicao <13718054285@163.com>
2025-05-03 13:20:05 +0800

    test again

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-    base: '/EmeraldKnight/',
+    base: '/EmeraldKnight/web/',
     plugins: [
         viteStaticCopy({
             targets: [
                 {
                     src: 'data/*',    // 你项目里的 data 文件夹
-                    dest: 'data'      // 输出到 dist/data/
+                    dest: './'      // 输出到 dist/
                 }
             ]
         })
