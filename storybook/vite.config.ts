31e17927c934e9fd71bd553176218907c0eddb7f
tuzicao <13718054285@163.com>
2025-05-12 17:02:11 +0800

    部署编辑器

--- storybook/vite.config.ts
+++ storybook/vite.config.ts
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/EmeraldKnight/',
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
 })
