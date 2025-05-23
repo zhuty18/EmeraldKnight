23a01aa1781f026a316d244a5153b98a8adfb411
tuzicao <13718054285@163.com>
2025-05-23 23:41:58 +0800

    从库里删除lock

--- storybook/vite.config.ts
+++ storybook/vite.config.ts
@@ -1,16 +1,17 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
     base: "/EmeraldKnight/storybook/",
-    plugins: [vue(), vueDevTools()],
+    plugins: [tailwindcss(), vue(), vueDevTools()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-});
+})
